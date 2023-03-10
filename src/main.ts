b654a3d93ca4c68bfc5b179ddd555606d1a2165c
Pier Fumagalli <pier@usrz.com>
2023-03-10 19:34:13 +0100

    Exit with ZERO only if changes were applied

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import upgradePackages from './index'
+import upgradePackages from './updater'
 
 import { ReleaseType } from 'semver'
 import yargs from 'yargs'
@@ -11,26 +11,35 @@ import yargs from 'yargs'
 const parsed = yargs
   .usage('$0 [--options ...] [package.json ...]')
   .help('h').alias('h', 'help').alias('v', 'version')
-  .option('strict', {
-    alias: 's',
+  .option('s', {
+    alias: 'strict',
     type: 'boolean',
     description: [
       'Strictly adhere to semver rules for tilde (~x.y.z)',
       'and caret (^x.y.z) dependency ranges',
     ].join('\n'),
   })
-  .option('debug', {
-    alias: 'd',
+  .option('q', {
+    alias: 'quick',
+    type: 'boolean',
+    description: [
+      'Consider dev/peer/optional dependency updates if and',
+      'only if the main depenencies also had updates',
+    ].join('\n'),
+  })
+  .option('d', {
+    alias: 'debug',
     type: 'boolean',
     description: 'Output debugging informations',
   })
-  .options('bump', {
-    alias: 'b',
+  .options('b', {
+    alias: 'bump',
     coerce: ((bump): ReleaseType => bump == true ? 'patch' : bump),
     choices: [ 'major', 'minor', 'patch' ] as ReleaseType[],
     description: 'Bump the version of the package file on changes',
   })
-  .options('dryrun', {
+  .options('x', {
+    alias: 'dry-run',
     type: 'boolean',
     description: 'Only process changes without writing to disk',
   })
@@ -41,14 +50,18 @@ const parsed = yargs
   .strictOptions()
   .argv
 
-Promise.resolve(parsed).then(({ bump, strict, debug, dryrun, _: args = [] }) => {
+Promise.resolve(parsed).then(({ b: bump, s: strict, q: quick, d: debug, x: dryrun, _: args = [] }) => {
   /* Normalize arguments and default to package.json in the current directory */
   const files = args.map((arg) => arg.toString())
   if (! files.length) files.push('package.json')
 
   /* Process packages, one by one */
-  upgradePackages(files, { strict, debug, dryrun, bump }).catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+  upgradePackages(files, { strict, quick, debug, dryrun, bump })
+    .then((changes) => {
+      process.exit(changes ? 0 : 1)
+    })
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
 }).catch((error) => console.log(error))
