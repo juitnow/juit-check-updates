ec7744cf16375d51e9fd3169f577f748f94ff175
Pier Fumagalli <pier@usrz.com>
2023-03-21 00:40:28 +0100

    All is ESM!

--- build.ts
+++ build.ts
@@ -2,16 +2,16 @@ import { build, find, tasks, bundleLocals, banner } from '@plugjs/build'
 
 export default build({
   ...tasks({
-    'esmTranspile': false,
+    'cjsTranspile': false,
   }),
 
   async bundle(): Promise<void> {
     banner('Bundling output')
 
-    await find('dist/main.cjs')
+    await find('dist/main.mjs')
         .esbuild({
           bundle: true,
-          format: 'cjs',
+          format: 'esm',
           outfile: 'check-updates.js',
           platform: 'node',
           sourcemap: false,
