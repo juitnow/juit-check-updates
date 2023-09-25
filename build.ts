49bd425c785c6039970c528d022d422301b10c8e
Pier Fumagalli <pier@usrz.com>
2023-09-25 13:08:10 +0200

    Simplify build (standardize)

--- build.ts
+++ build.ts
@@ -1,28 +1,10 @@
-import { banner, bundleLocals, find, plugjs, tasks } from '@plugjs/build'
+import { plugjs, tasks } from '@plugjs/build'
 
 export default plugjs({
-  ...tasks({
-    'cjs': false,
-  }),
-
-  async bundle(): Promise<void> {
-    banner('Bundling output')
-
-    await find('dist/main.mjs')
-        .esbuild({
-          bundle: true,
-          format: 'esm',
-          outfile: 'check-updates.js',
-          platform: 'node',
-          sourcemap: false,
-          minify: false,
-          plugins: [ bundleLocals() ],
-        })
-  },
+  ...tasks(),
 
   async all(): Promise<void> {
     await this.transpile()
     await this.lint()
-    await this.bundle()
   },
 })
