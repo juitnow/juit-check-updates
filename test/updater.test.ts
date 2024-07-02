4ef817afb65da3e0cb7c7f662f85ce754859bedc
Pier Fumagalli <pier@usrz.com>
2024-07-02 13:53:36 +0200

    Test pre-releases upgrade

--- test/updater.test.ts
+++ test/updater.test.ts
@@ -1,6 +1,9 @@
 import * as plug from '@plugjs/build'
 import * as semver from 'semver'
 
+import { Updater } from '../src/updater'
+import { VersionsCache } from '../src/versions'
+
 
 describe('Dependencies Updater', () => {
   const scriptFile = plug.resolve('./src/main.mts')
@@ -52,5 +55,68 @@ describe('Dependencies Updater', () => {
     } finally {
       await plug.rmrf(tempdir)
     }
+  })
+
+  it('should not upgrade from release to pre-release', async () => {
+    const source = plug.resolve('test', 'package-release.json')
+
+    const cache = new VersionsCache()
+    ;(cache as any)._cache['test-dep'] = [ '0.9', '0.8', '0.7' ]
+    ;(cache as any)._cache['test-dep+prerelease'] = [ '1.0.0-beta.2', '1.0.0-beta.1' ]
+
+    const updater = new Updater(source, {
+      bump: 'patch',
+      debug: false,
+      quick: false,
+      strict: false,
+      workspaces: false,
+    }, cache)
+
+    await updater.init()
+    await updater.update()
+
+    expect((updater as any)._packageData.dependencies['test-dep']).toEqual('^1.0.0')
+  })
+
+  it('should upgrade from pre-release to pre-release', async () => {
+    const source = plug.resolve('test', 'package-prerelease.json')
+
+    const cache = new VersionsCache()
+    ;(cache as any)._cache['test-dep'] = [ '0.9', '0.8', '0.7' ]
+    ;(cache as any)._cache['test-dep+prerelease'] = [ '1.1.0-beta.3', '1.0.0-beta.2', '1.0.0-beta.1' ]
+
+    const updater = new Updater(source, {
+      bump: 'patch',
+      debug: false,
+      quick: false,
+      strict: false,
+      workspaces: false,
+    }, cache)
+
+    await updater.init()
+    await updater.update()
+
+    expect((updater as any)._packageData.dependencies['test-dep']).toEqual('^1.0.0-beta.2')
+  })
+
+  it('should stop upgrading pre-releases at the next release', async () => {
+    const source = plug.resolve('test', 'package-prerelease.json')
+
+    const cache = new VersionsCache()
+    ;(cache as any)._cache['test-dep'] = [ '1.1.0', '1.0.0', '0.9.0', '0.8.0', '0.7.0' ]
+    ;(cache as any)._cache['test-dep+prerelease'] = [ '1.1.0-beta.3', '1.0.0-beta.2', '1.0.0-beta.1' ]
+
+    const updater = new Updater(source, {
+      bump: 'patch',
+      debug: false,
+      quick: false,
+      strict: false,
+      workspaces: false,
+    }, cache)
+
+    await updater.init()
+    await updater.update()
+
+    expect((updater as any)._packageData.dependencies['test-dep']).toEqual('^1.1.0')
   })
 })
